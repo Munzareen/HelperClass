f26b967d6b180098e652d818ae4575ee2b9cc2a0
Munzareen <munzareenatique@gmail.com>
2019-11-01 12:36:34 +0500

    new

---
+++ Helper_Class/Helper_Class/SharedHelper.swift
@@ -0,0 +1,30 @@
+//
+//  SharedHelper.swift
+//  Helper_Class
+//
+//  Created by Munzareen Atique on 11/1/19.
+//  Copyright © 2019 Munzareen Atique. All rights reserved.
+//
+
+import UIKit
+
+class SharedHelper: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
